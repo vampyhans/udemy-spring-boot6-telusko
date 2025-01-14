8fcf917c46138d5bcb465a1e562f8883e7c0b80b
Hansa Wijesinghe <hansahimasarawijesinghe@gmail.com>
2025-01-14 10:11:47 +0530

    spring boot setup

---
+++ src/main/java/com/udemy_spring_boot6_telusko/app/UdemySpringBoot6TeluskoApplication.java
@@ -0,0 +1,13 @@
+package com.udemy_spring_boot6_telusko.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringBoot6TeluskoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringBoot6TeluskoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy_spring_boot6_telusko/app/UdemySpringBoot6TeluskoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy_spring_boot6_telusko.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringBoot6TeluskoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
